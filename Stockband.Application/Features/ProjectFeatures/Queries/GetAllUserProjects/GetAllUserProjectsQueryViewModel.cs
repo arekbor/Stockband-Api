d36ffe216761f758642c1bee4097cb35188ab2dc
arekbor <arekindependent@gmail.com>
2023-07-14 23:34:51 +0200

    add project id to view model

--- Stockband.Application/Features/ProjectFeatures/Queries/GetAllUserProjects/GetAllUserProjectsQueryViewModel.cs
+++ Stockband.Application/Features/ProjectFeatures/Queries/GetAllUserProjects/GetAllUserProjectsQueryViewModel.cs
@@ -3,6 +3,7 @@ namespace Stockband.Application.Features.ProjectFeatures.Queries.GetAllUserProje
 public class GetAllUserProjectsQueryViewModel
 {
     public int OwnerId { get; set; }
+    public int ProjectId { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
 }
